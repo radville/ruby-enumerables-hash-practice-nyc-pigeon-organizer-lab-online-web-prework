18e1c90d9d878c6d73779bd4cf08659311efa982
Laura Radville <laura.radville@gmail.com>
2019-07-02 15:23:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   hash = Hash.new(0)
   data.each do |el|
-    el[1].each do |inner_el|
+    el[1].keys.each do |keys|
+      el[keys]
     binding.pry
     end
   end
